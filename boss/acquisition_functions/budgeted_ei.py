Fix cost function's input broadcasting

boss/acquisition_functions/budgeted_ei.py
@@ -65,10 +65,12 @@ class BudgetedExpectedImprovement(AnalyticAcquisitionFunction):
             design points `X`.
         """
         posterior = self._get_posterior(X)
-        mean = posterior.mean  # (b) x 1
-        sigma = posterior.variance.sqrt().clamp_min(1e-6)  # (b) x 1
+        mean = posterior.mean.squeeze(dim=-1)  # (b) x 1
+        sigma = posterior.variance.clamp_min(1e-6).sqrt().squeeze(dim=-1)  # (b) x 1
 
         # (b) x 1
+        # print(mean.shape)
+        # print(self.best_f.shape)
         u = (mean - self.best_f) / sigma
 
         if not self.maximize:
@@ -80,10 +82,12 @@ class BudgetedExpectedImprovement(AnalyticAcquisitionFunction):
         pdf_u = torch.exp(standard_normal.log_prob(u))
         cdf_u = standard_normal.cdf(u)
         ei = sigma * (pdf_u + u * cdf_u)  # (b) x 1
-        # (b) x 1
-        cost = self.cost_function(X)
-        smooth_feas_ind = soft_eval_constraint(cost - self.budget)
-        bc_ei = ei.mul(smooth_feas_ind)  # (b) x 1
+        # print(ei.shape)
+        cost = self.cost_function(X)  # (b) x 1 x 1
+        smooth_feas_ind = soft_eval_constraint(cost - self.budget)  # (b) x 1 x 1
+        bc_ei = ei.mul(smooth_feas_ind.squeeze(dim=-1))  # (b) x 1
+        # print(X.shape)
+        # print(bc_ei.shape)
         return bc_ei.squeeze(dim=-1)
 
 

boss/acquisition_functions/budgeted_multi_step_ei.py
@@ -184,6 +184,7 @@ def _step(
     Returns:
         A `b`-dim tensor containing the multi-step value of the design `X`.
     """
+    # print(step_index)
     X = Xs[0]
     if sample_weights is None:  # only happens in the initial step
         sample_weights = torch.ones(*X.shape[:-2], device=X.device, dtype=X.dtype)
@@ -195,7 +196,7 @@ def _step(
         X=X,
         objective=objective,
         inner_sampler=inner_samplers[0],
-        arg_fac=valfunc_argfacs[0](budget),
+        arg_fac=valfunc_argfacs[0](cost_function, budget),
     )
     if stage_val is not None:  # update running value
         # if not None, running_val has shape f_{i-1} x ... x f_1 x batch_shape
@@ -251,15 +252,16 @@ def _step(
 class budgeted_ei_argfac(Module):
     r"""Extract the best observed value and reamaining budget from the model."""
 
-    def __init__(self, budget: Union[float, Tensor]) -> None:
+    def __init__(self, cost_function: Callable, budget: Union[float, Tensor]) -> None:
         super().__init__()
+        self.cost_function = cost_function
         self.budget = budget
 
     def forward(self, model: Model, X: Tensor) -> Dict[str, Any]:
-        y = torch.transpose(model.train_targets, -2, -1)
-        y_original_scale = model.outcome_transform.untransform(y)[0]
-        obj_vals = y_original_scale[..., 0]
+        obj_vals_transformed = model.train_targets
+        obj_vals = model.outcome_transform.untransform(obj_vals_transformed)[0]
         params = {
+            "cost_function": self.cost_function,
             "best_f": obj_vals.max(dim=-1, keepdim=True).values,
             "budget": self.budget,
         }

boss/boss_trial.py
@@ -18,7 +18,6 @@ from boss.acquisition_functions.budgeted_multi_step_ei import (
 from boss.utils import (
     fit_model,
     generate_initial_design,
-    get_suggested_budget,
     optimize_acqf_and_get_suggested_point,
 )
 
@@ -124,40 +123,22 @@ def boss_trial(
 
         # New suggested point
         t0 = time.time()
-        try:
-            new_x = get_new_suggested_point(
-                algo=algo,
-                X=X,
-                Y=Y,
-                costs=costs,
-                cost_function=cost_function,
-                budget_left=budget - cumulative_cost,
-                algo_params=algo_params,
-            )
-        except:
-            if ignore_failures:
-                print(
-                    "An error ocurred when computing the next point to evaluate. Instead, a point will be chosen uniformly at random."
-                )
-                new_x = get_new_suggested_point(
-                    algo="Random",
-                    X=X,
-                    Y=Y,
-                    costs=costs,
-                    budget_left=budget - cumulative_cost,
-                    algo_params=algo_params,
-                )
-            else:
-                raise Error(
-                    "An error ocurred when computing the next point to evaluate."
-                )
+        new_x = get_new_suggested_point(
+            algo=algo,
+            X=X,
+            Y=Y,
+            costs=costs,
+            cost_function=cost_function,
+            budget_left=budget - cumulative_cost,
+            algo_params=algo_params,
+        )
 
         t1 = time.time()
         runtimes.append(t1 - t0)
 
         # Evaluate objective at new point
         objective_new_x = objective_function(new_x)
-        cost_new_x = cost_function(new_x)
+        cost_new_x = cost_function(new_x.unsqueeze(0)).item()
         # Update training data
         X = torch.cat([X, new_x], 0)
         Y = torch.cat([Y, objective_new_x], 0)
@@ -165,7 +146,7 @@ def boss_trial(
         cost_function = cost_function.update_reference_point(X[[-1]])
 
         # Update historical best observed objective values and cumulative cost
-        cumulative_cost += cost_new_x.item()
+        cumulative_cost += cost_new_x
         best_obs_val = Y.max().item()
         hist_best_obs_vals.append(best_obs_val)
         print("Best value found so far: " + str(best_obs_val))
@@ -224,10 +205,11 @@ def get_new_suggested_point(
         )
 
         # Acquisition function
+        budget = torch.clone(torch.tensor(budget_left)).clamp_max(10.0)
         acquisition_function = BudgetedMultiStepExpectedImprovement(
             model=model,
             cost_function=cost_function,
-            budget=torch.clone(torch.tensor(budget_left)),
+            budget=budget,
             num_fantasies=algo_params.get("lookahead_n_fantasies"),
         )
     elif algo == "EI":

experiments/dropwave_runner.py
@@ -8,6 +8,7 @@ from botorch.test_functions.synthetic import DropWave
 from torch import Tensor
 
 torch.set_default_dtype(torch.float64)
+torch.autograd.set_detect_anomaly(True)
 debug._set_state(True)
 
 script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -27,14 +28,27 @@ def objective_function(X: Tensor) -> Tensor:
 
 
 def cost_function_callable(reference_point: Tensor, X: Tensor) -> Tensor:
-    cost_X = torch.cdist(
-        reference_point.view(
-            torch.Size([1]) + reference_point.shape[:-2] + reference_point.shape[-1:]
-        ),
-        X.view(torch.Size([1]) + X.shape[:-2] + X.shape[-1:]),
+    # print("TEST 1 BEGINS")
+    # print(reference_point.shape)
+    # print(X.shape)
+    # print(X.shape[:1] + torch.Size([-1] * (len(X.shape) - 1)))
+    cost_X = (
+        10.24
+        * (
+            torch.linalg.norm(
+                reference_point.unsqueeze(0).repeat(
+                    X.shape[:1] + torch.Size([1] * (len(X.shape) - 1))
+                )
+                - X,
+                dim=-1,
+                keepdim=True,
+            )
+        )
+        + 1.0
     )
-    cost_X *= 10.24
-    cost_X += 0.1
+    # print(cost_X.shape)
+    # print("TEST 1 ENDS")
+    # print(cost_X)
     return cost_X
 
 
@@ -44,7 +58,7 @@ cost_function = GenericCostFunction(cost_function_callable)
 # Algos
 algo = "B-MS-EI"
 if algo == "B-MS-EI":
-    algo_params = {"lookahead_n_fantasies": [1, 1, 1]}
+    algo_params = {"lookahead_n_fantasies": [1, 1]}
 else:
     algo_params = {}
 
@@ -67,5 +81,5 @@ experiment_manager(
     cost_function=cost_function,
     input_dim=2,
     n_init_evals=6,
-    budget=10.0,
+    budget=50.0,
 )